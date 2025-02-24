48457d5b3a6543be32a3843660180761b19e6773
Arun Ayyankave <arunayk712@gmail.com>
2025-02-25 02:19:59 +0530

    second

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,10 @@ const userRouter = express.Router()
 const userController = require("../controllers/userController")
 const auth = require('../middlewares/userAuth')
 
+userRouter.get("/",(req,res)=>{
+    res.send("hello welcome to backend api")
+})
+
 userRouter.post('/signup',userController.signup)
 userRouter.post('/login', userController.login)
 userRouter.get('/home',auth, userController.home)
